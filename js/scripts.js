21f4b1e1eea6b1c4a1df8c94b15e97520c82daf5
babel-lab <30329832+babel-lab@users.noreply.github.com>
2017-09-05 16:24:04 +0800

    Babel's Portfolio
    Babel's Portfolio: Ver. 1

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+﻿  //畫面縮小時選單變化
+  var num = 50; //number of pixels before modifying styles
+  $(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('#top').addClass('scrolled');
+    } else {
+        $('#top').removeClass('scrolled');
+    }
+  });
+  
+  //回最上面
+  var offset = 200;
+  var duration = 500;
+  $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(400);
+        } else {
+            $('.back-to-top').fadeOut(400);
+        }
+    });
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 600);
+        return false;
+  });
